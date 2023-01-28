c37887d36c8b918a1fb60ced299e28b39a7f56d9
Joey Van Lierop <joey.vanlierop@me.com>
2023-01-28 14:52:15 -0700

    Document logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,6 +2,15 @@
 #include <iomanip>
 #include "logger.h"
 
+/**
+ * Log a message to an output stream along with the file name, line, and column of the error
+ * @param ostream output stream to log the message to
+ * @param filereader FileReader object containing the file name and stream
+ * @param line line number of the error
+ * @param column column number of the error
+ * @param width width of the error mark
+ * @param message the message to log
+ */
 void log(std::ostream& ostream, FileReader* filereader, int line, int column, int width, std::string message) {
     // File name and location
     ostream << "--> " << filereader->filename << ":" << line << ":" << column << std::endl;
@@ -20,24 +29,56 @@ void log(std::ostream& ostream, FileReader* filereader, int line, int column, in
     print_line(ostream, message);
 }
 
+/**
+ * Log a warning message to the standard error stream
+ * @param filereader FileReader object containing the file name and stream
+ * @param line line number of the error
+ * @param column column number of the error
+ * @param width width of the error mark
+ * @param message the warning message to log
+ */
 void warning(FileReader* filereader, int line, int column, int width, std::string message) {
     log(std::cerr, filereader, line, column, width, "warning: " + message);
 }
 
+/**
+ * Log an error message to the standard error stream and exits the program
+ * @param filereader FileReader object containing the file name and stream
+ * @param line line number of the error
+ * @param column column number of the error
+ * @param width width of the error mark
+ * @param message the error message to log
+ */
 void error(FileReader* filereader, int line, int column, int width, std::string message) {
     log(std::cerr, filereader, line, column, width, "error: " + message);
     exit(EXIT_FAILURE);
 }
 
+/**
+ * Print a string to an output stream with an aligned vertical line
+ * @param ostream output stream to print the line to.
+ * @param content the string to print.
+ */
 void print_line(std::ostream& ostream, std::string content) {
     ostream << std::setw(2) << " " << "| " << content << std::endl;
 }
 
+/**
+ * Print a string to an output stream with an aligned vertical line and line number
+ * @param ostream output stream to print the line to
+ * @param content the string to print
+ * @param line line number to print
+ */
 void print_line(std::ostream& ostream, std::string content, int line) {
     ostream << std::setw(2) << std::left << line << "| " << content << std::endl;
 }
 
-
+/**
+ * Takes in an input file stream and a line number as input, and returns the content of that line as a string
+ * @param filestream the input file stream to read from
+ * @param line the line number to get the content from
+ * @return a string representing the specified line of the input file
+*/
 std::string get_line(std::ifstream& filestream, int line) {
     std::string out;
     filestream.seekg(std::ios::beg);
