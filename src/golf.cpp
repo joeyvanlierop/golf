3976c099423a3a4ac9e333c0edb2dd48db10f853
Joey Van Lierop <joey.vanlierop@me.com>
2023-02-03 11:16:17 -0700

    Validate input

--- src/golf.cpp
+++ src/golf.cpp
@@ -3,7 +3,14 @@
 #include "lexer.h"
 #include "filereader.h"
 
-int main(int, char* argv[]) {
+int main(int argc, char* argv[]) {
+    // Validate input
+    if (argc < 2)
+    {
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     // Read input
     FileReader reader(argv[1]);
 
